5b029d70830bee139e3443f3d2d6944e3d455c50
Jake Bellacera <hi@jakebellacera.com>
2013-01-14 13:03:05 -0800

    Simplifying the first exercise

--- 1-multiples_of_3_and_5.rb
+++ 1-multiples_of_3_and_5.rb
@@ -2,12 +2,6 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum = 0
-
-1000.times do |i|
-  if i % 3 == 0 || i % 5 == 0
-    sum += i
-  end
-end
-
-puts "The anwser is: #{sum}"
+total = 0
+1000.times { |i| total += i if i % 3 == 0 || i % 5 == 0 }
+puts "The anwser is: #{total}"
